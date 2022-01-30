6cb72f3702d8f489dc1ddfe63891611b161d95c4
Destin Lee <destin.lee@johnsonfit.com>
2022-01-30 13:14:39 -0600

    100vh fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { CgYinyang } from 'react-icons/cg'
 const StyledHome = styled.main`
   max-width: 900px;
   width: 100%;
-  height: 80%;
+  height: 100vh;
   padding: 0 2rem;
   margin: 0 auto;
   display: flex;
