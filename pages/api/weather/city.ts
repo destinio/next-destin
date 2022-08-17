0b7f5e6eb413e4d4333efcdadc17dbf6b7f1ceba
Destin Lee <destin.lee@johnsonfit.com>
2022-08-17 09:13:55 -0500

    weather api

---
+++ pages/api/weather/city.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function searchHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const { sk } = req.body as { sk: string }
+    // const cleanTerm = term.toLowerCase().split(' ').join('%20')
+
+    // console.log(req.body)
+
+    fetch(
+      `http://dataservice.accuweather.com/currentconditions/v1/${sk}?apikey=${process.env.WEATHER_API_KEY}`
+    )
+      .then(res => res.json())
+      .then(data => res.json(data))
+  }
+}
---
+++ pages/api/weather/search.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function searchHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const { term } = req.body as { term: string }
+    const cleanTerm = term.toLowerCase().split(' ').join('%20')
+
+    console.log(req.body)
+
+    fetch(
+      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${process.env.WEATHER_API_KEY}&q=${cleanTerm}`
+    )
+      .then(res => res.json())
+      .then(data => res.json(data))
+  }
+}
